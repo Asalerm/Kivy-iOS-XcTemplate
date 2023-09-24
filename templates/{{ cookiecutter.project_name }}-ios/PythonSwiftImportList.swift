6d7da36ca0af4c446bb3f880987c8119863b6b44
psychowasp <psychowaspx@gmail.com>
2023-09-24 17:18:44 +0200

    init

---
+++ templates/{{ cookiecutter.project_name }}-ios/PythonSwiftImportList.swift
@@ -0,0 +1,13 @@
+// PythonSwiftImportList.swift
+import Foundation
+import PythonSwiftCore
+
+
+let PythonSwiftImportList: [PySwiftModuleImport] = [
+    //insert PySwiftModule import functions here
+    //(makeCString(from: "tts"), PyInit_tts),
+    //(makeCString(from: "speech_recoginizer"), PyInit_speech_recognizer)
+    
+    
+    
+]
